bae921d5c5905dcc5c4a847dfe00cc50af6572c7
hlr0 <160110248+hlr0@users.noreply.github.com>
2025-02-27 11:23:27 +0200

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -148,21 +148,22 @@ echo 1 > /proc/sys/net/ipv4/conf/all/log_martians
 
 #!/bin/bash
 IPT="/sbin/iptables"
+
 ######---------------------------------------------------------------------------------
 ######-----/// SETTINGS OF SYSTEM
 ######---------------------------------------------------------------------------------
 echo -e "------------\n Setting Network Cards \n------------\n"
-NETIF_0="eth0"  	# Update to use your first network card
-NETIF_1="eth1"  	# Update to use your second network card
+NETIF_0="eth0"  # Update to use your first network card
+NETIF_1="eth1"  # Update to use your second network card
 
 ######---------------------------
-echo -e "------------\n Setting your DNS servers can use cat /etc/resolv.conf \n------------\n"
+echo -e "------------\n Setting your DNS servers \n------------\n"
 DNS_SERVER="9.9.9.9 8.8.8.8 1.1.1.1"
 
 ######---------------------------
 echo -e "------------\n Getting Server IP \n------------\n"
-SERVER_IP_0="$(ip addr show $NETIF_0 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
-SERVER_IP_1="$(ip addr show $NETIF_1 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
+SERVER_IP_0="$(ip -4 addr show $NETIF_0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
+SERVER_IP_1="$(ip -4 addr show $NETIF_1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
 
 ######---------------------------------------------------------------------------------
 ######-----/// START OF SCRIPT FOR IPTABLES
@@ -172,10 +173,10 @@ $IPT -F
 $IPT -X
 $IPT -t filter -F
 $IPT -t filter -X
-$IPT -t mangle -F
-$IPT -t mangle -X
 $IPT -t nat -F
 $IPT -t nat -X
+$IPT -t mangle -F
+$IPT -t mangle -X
 $IPT -t raw -F
 $IPT -t raw -X
 $IPT -t security -F
@@ -196,34 +197,27 @@ $IPT -A OUTPUT -o lo -j ACCEPT
 
 ######---------------------------
 echo -e "------------\n Ping from inside to outside \n------------\n"
-$IPT -A OUTPUT -p icmp -i $NETIF_0 --icmp-type echo-request -j ACCEPT
-$IPT -A INPUT -p icmp -o $NETIF_0 --icmp-type echo-reply -j ACCEPT
+$IPT -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
+$IPT -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
 ######---------------------------
 echo -e "------------\n Ping from outside to inside \n------------\n"
-$IPT -A INPUT -p icmp -i $NETIF_0 --icmp-type echo-request -j ACCEPT
-$IPT -A OUTPUT -p icmp -o $NETIF_0 --icmp-type echo-reply -j ACCEPT
+$IPT -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+$IPT -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
 ######---------------------------
-#echo -e "------------\n Allow outbound DNS \n------------\n"
-#$IPT -A OUTPUT -p udp -o $NETIF_0 --dport 53 -j ACCEPT
-#$IPT -A INPUT -p udp -i $NETIF_0 --sport 53 -j ACCEPT
-
-######---------------------------
-echo -e "------------\n Allow DNS IPADDR \n------------\n"
-for dnsip in $DNS_SERVER
-do
-    echo -e "------------\n Allowing DNS lookups (tcp, udp port 53) to server '$dnsip' \n------------\n"
-    $IPT -A OUTPUT -p udp -o $NETIF_0 -d $dnsip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-    $IPT -A INPUT  -p udp -i $NETIF_0 -s $dnsip --sport 53 -m state --state ESTABLISHED     -j ACCEPT
-    $IPT -A OUTPUT -p tcp -o $NETIF_0 -d $dnsip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-    $IPT -A INPUT  -p tcp -i $NETIF_0 -s $dnsip --sport 53 -m state --state ESTABLISHED     -j ACCEPT
+echo -e "------------\n Allow DNS Requests \n------------\n"
+for dnsip in $DNS_SERVER; do
+    $IPT -A OUTPUT -p udp -d $dnsip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+    $IPT -A INPUT  -p udp -s $dnsip --sport 53 -m state --state ESTABLISHED -j ACCEPT
+    $IPT -A OUTPUT -p tcp -d $dnsip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+    $IPT -A INPUT  -p tcp -s $dnsip --sport 53 -m state --state ESTABLISHED -j ACCEPT
 done
 
 ######---------------------------
-echo -e "------------\n Allow previously established connections to continue uninterupted \n------------\n"
-$IPT -A INPUT -o $NETIF_0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-$IPT -A OUTPUT -i $NETIF_0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+echo -e "------------\n Allow Established Connections \n------------\n"
+$IPT -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+$IPT -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 ######---------------------------
 echo -e "------------\n Allow outgoing SSH \n------------\n"
@@ -231,44 +225,34 @@ $IPT -A OUTPUT -o $NETIF_0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j
 $IPT -A INPUT -i $NETIF_0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
 ######---------------------------
-echo -e "------------\n Allow outbound connections on the ports specific ports. \n------------\n"
-$IPT -A OUTPUT -p tcp -o $NETIF_0 --dport 53 -j ACCEPT #DNS
-$IPT -A OUTPUT -p udp -o $NETIF_0 --dport 53 -j ACCEPT #DNS
-$IPT -A OUTPUT -p tcp -o $NETIF_0 --dport 80 -j ACCEPT #HTTP
-$IPT -A OUTPUT -p tcp -o $NETIF_0 --dport 443 -j ACCEPT #HTTPS
-$IPT -A OUTPUT -p UDP -o $NETIF_0 --dport 67:68 -j ACCEPT #DHCP
+echo -e "------------\n Allow HTTP and HTTPS \n------------\n"
+$IPT -A OUTPUT -p tcp -o $NETIF_0 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+$IPT -A OUTPUT -p tcp -o $NETIF_0 --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 ######---------------------------
 echo -e "------------\n Prevent DoS attack \n------------\n"
-# Implementing SYN flood protection
 $IPT -A INPUT -p tcp --syn -m limit --limit 1/s --limit-burst 5 -j ACCEPT
-$IPT -A INPUT -p tcp --syn -j DROP # Drop excess SYN packets to prevent DoS attacks
+$IPT -A INPUT -p tcp --syn -j DROP
 
 ######---------------------------
-echo -e "------------\n Set up logging for incoming and outgoing traffic. \n------------\n"
-# Create a custom chain for logging and dropping
+echo -e "------------\n Logging and Dropping Unwanted Traffic \n------------\n"
 $IPT -N LOGNDROP
-# Add rules to log incoming traffic before dropping it
-$IPT -A INPUT -j LOGNDROP
-$IPT -A LOGNDROP -m limit --limit 5/min -j LOG
+$IPT -A LOGNDROP -m limit --limit 5/min -j LOG --log-prefix "IPTABLES DROP: " --log-level 4
 $IPT -A LOGNDROP -j DROP
-# Log incoming traffic before dropping
-$IPT -A INPUT -j LOG -m limit --limit 12/min --log-level 4 --log-prefix 'IP INPUT drop: '
-$IPT -A INPUT -j DROP
-# Log outgoing traffic before dropping
-$IPT -A OUTPUT -j LOG -m limit --limit 12/min --log-level 4 --log-prefix 'IP OUTPUT drop: '
-$IPT -A OUTPUT -j DROP
 
-######---------------------------
-#echo -e "------------\n Blocking specific IPADDRS \n------------\n"
-#BLOCK_THESE_IPS="x.x.x.x x.x.x.x x.x.x.x"
-#for blockip in $BLOCK_THESE_IPS
-#do
-#    echo -e "------------\n Blocking the IPADDR '$blockip' \n------------\n"
-#    $IPT -A INPUT -s "$blockip" -j DROP
-#done
+$IPT -A INPUT -j LOGNDROP
+$IPT -A OUTPUT -j LOGNDROP
 
 echo -e "######---------------------------\n"
 echo -e "######-----/// EOF SCRIPT\n"
 echo -e "######---------------------------\n"
+
+##################------------------------------
+####----//FIX TO REVERT BACK
+##################------------------------------
+#iptables -F
+#iptables -X
+#iptables -P INPUT ACCEPT
+#iptables -P OUTPUT ACCEPT
+#iptables -P FORWARD ACCEPT
 exit 0
